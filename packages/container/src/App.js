06ee0e4a866fa905a4eb77e41e0d46caeb99d4aa
Jai <jai.gupta@apollo247.org>
2024-08-17 21:56:48 +0530

    update text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>Hey Container 278</h1>
+        <h1>Hey Container !!!!!!!!!!!!!!!!!</h1>
         <hr />
         <MarketingApp />
     </div>
