4d26d952dd842595af45a4bb754202ddeddf44e4
Jai <jai.gupta@apollo247.org>
2024-08-17 19:35:24 +0530

    update container test 2

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "./components/MarketingApp";
 
 export default () => {
     return <div>
-        <h1>Hey Container 222</h1>
+        <h1>Hey Container 27</h1>
         <hr />
         <MarketingApp />
     </div>
